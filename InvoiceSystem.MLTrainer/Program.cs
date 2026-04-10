dd7e45e44c40b964645fcf96ec3a1902afe22ec6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-04-10 12:01:01 +0800

    - registering DI and Runner for the csv generator - WIP

--- InvoiceSystem.MLTrainer/Program.cs
+++ InvoiceSystem.MLTrainer/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InvoiceSystem.Application.Common.Models.ML.DataProviders;
+using InvoiceSystem.Infrastructure.MachineLearning.DataProviders;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddScoped<IRiskTrainingDataProvider, InvoiceRiskDataProvider>();
+    }).RunConsoleAsync();
