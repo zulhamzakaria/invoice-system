0d787c0fa6a949a40722b332954c10349d38f709
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-04-11 08:46:12 +0800

    - WIP

---
+++ InvoiceSystem.MLTrainer/Runner.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace InvoiceSystem.MLTrainer;
+
+public interface IRunner
+{
+    Task<int> RunAsync(CancellationToken ct);
+}
+
+public class Runner : IRunner
+{
+    private readonly ILogger<Runner> _logger;
+    public Runner(ILogger<Runner> logger)
+    {
+        _logger = logger;
+    }
+    public Task<int> RunAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
