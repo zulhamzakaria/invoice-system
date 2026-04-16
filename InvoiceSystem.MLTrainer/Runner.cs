25cf6f6dc6317bacb4234fd92a0868a0d6b73d22
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-04-16 11:32:22 +0800

    - wIP

--- InvoiceSystem.MLTrainer/Runner.cs
+++ InvoiceSystem.MLTrainer/Runner.cs
@@ -2,18 +2,8 @@
 
 namespace InvoiceSystem.MLTrainer;
 
-public interface IRunner
+public class Runner(ILogger<Runner> logger)
 {
-    Task<int> RunAsync(CancellationToken ct);
-}
-
-public class Runner : IRunner
-{
-    private readonly ILogger<Runner> _logger;
-    public Runner(ILogger<Runner> logger)
-    {
-        _logger = logger;
-    }
     public Task<int> RunAsync(CancellationToken ct)
     {
         throw new NotImplementedException();
