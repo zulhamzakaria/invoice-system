e20236273fbca576fdac40d4d8a907aa98d1ebdf
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-04-17 10:55:07 +0800

    - WIP

--- InvoiceSystem.MLTrainer/Runner.cs
+++ InvoiceSystem.MLTrainer/Runner.cs
@@ -1,6 +1,7 @@
 ﻿using InvoiceSystem.Application.Common.Models.ML.DataProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Runtime.CompilerServices;
 
 namespace InvoiceSystem.MLTrainer;
 
@@ -10,12 +11,15 @@ public class Runner
     IOptions<MLSettings> mlSettings,
     IRiskModelTrainer riskModelTrainer)
 {
+
+    private readonly MLSettings _settings = mlSettings.Value;
     public Task<int> RunAsync(CancellationToken ct)
     {
+         
         //// Safer hardcoding
         //var dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ML", "Data", "risk-training-data.csv");
-        var dataPath = mlSettings.Value.CsvFilePath;
-        var modelPath = mlSettings.Value.ModelFilePath;
+        var dataPath = _settings.CsvFilePath;
+        var modelPath = _settings.ModelFilePath;
 
 
         throw new NotImplementedException();
