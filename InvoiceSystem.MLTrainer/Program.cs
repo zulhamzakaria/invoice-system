4b991ee9f8246432ebe3e72f2cbde37f279e3cec
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-04-17 18:13:31 +0800

    - wired up

--- InvoiceSystem.MLTrainer/Program.cs
+++ InvoiceSystem.MLTrainer/Program.cs
@@ -1,18 +1,31 @@
 ﻿using InvoiceSystem.Application.Common.Models.ML.DataProviders;
+using InvoiceSystem.Infrastructure;
 using InvoiceSystem.Infrastructure.MachineLearning.DataProviders;
 using InvoiceSystem.MLTrainer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.Configure<MLSettings>
     (builder.Configuration.GetSection("MLSettings"));
+
+builder.Services.AddScoped<IRiskTrainingDataProvider, InvoiceRiskDataProvider>();
+builder.Services.AddScoped<IRiskModelTrainer, RiskModelTrainer>();
 builder.Services.AddScoped<Runner>();
 
+using var host = builder.Build();
 
-await Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
-    {
-        services.AddScoped<IRiskTrainingDataProvider, InvoiceRiskDataProvider>();
-    }).RunConsoleAsync();
+var runner = host.Services.GetRequiredService<Runner>();
+var exitCode = await runner.RunAsync(CancellationToken.None);
+
+Environment.Exit(exitCode);
+
